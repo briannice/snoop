93d54b93583e340b1758b738e7da58f268a2e2fa
Maxim Massart <maxim.massart@student.kdg.be>
2022-10-02 22:44:57 +0200

    Added simple backend for search of nameservers with dnspython library

---
+++ lib/domainresearch/__init__.py
@@ -0,0 +1,1 @@
+from .nslookup import lookupNsRecord
--- lib/domainresearch/nslookup.py
+++ lib/domainresearch/nslookup.py
@@ -1,7 +1,14 @@
-from nslookup import Nslookup
+import dns.resolver
+import dns.message
+import dns.rdataclass
+import dns.rdatatype
+import dns.query
 
-class NsLookupDomain():
-    def __init__(
-        self
-    ):
 
+def lookupNsRecord(domain):
+    qdomain = dns.name.from_text(domain)
+    q = dns.message.make_query(qdomain, dns.rdatatype.NS)
+    # todo: maybe add to choose DNS server instead of 8.8.8.8?
+    r = dns.query.udp(q, "8.8.8.8")
+    ns_rrset = r.find_rrset(r.answer, qdomain, dns.rdataclass.IN, dns.rdatatype.NS)
+    return ns_rrset
