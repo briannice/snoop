138e47f9314488cf4246ea23fe4e19e270cd4570
Maxim Massart <maxim.massart@student.kdg.be>
2022-10-03 21:41:19 +0200

    Added frontend layout and all possible options. Finish of whole frontend.

--- ui/tabs/nslookup_tab.py
+++ ui/tabs/nslookup_tab.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout, QHBoxLayout, QVBoxLayout, QLineEdit, QPushButton, QCheckBox, \
+    QTextBrowser
 
 
 class NslookupTab(QWidget):
@@ -6,7 +7,43 @@ class NslookupTab(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.Label = QLabel("Nslookup")
-        self.Layout = QGridLayout()
-        self.Layout.addWidget(self.Label)
+        # domain input
+        self.NsLabel = QLabel("Domain:")
+        self.DomainEdit = QLineEdit()
+        self.DomainEdit.setFixedWidth(200)
+        self.DomainButton = QPushButton("Scan")
+
+        self.Header = QHBoxLayout()
+        self.Header.addWidget(self.NsLabel)
+        self.Header.addSpacing(10)
+        self.Header.addWidget(self.DomainEdit)
+        self.Header.addSpacing(10)
+        self.Header.addWidget(self.DomainButton)
+        self.Header.addStretch(1)
+
+        # possible options
+        self.CheckboxNs = QCheckBox("NS")
+        self.CheckboxA = QCheckBox("A")
+        self.CheckboxAAAA = QCheckBox("AAAA")
+        self.CheckboxMx = QCheckBox("MX")
+
+        self.CheckBoxInput = QHBoxLayout()
+        self.CheckBoxInput.addWidget(self.CheckboxNs)
+        self.CheckBoxInput.addWidget(self.CheckboxA)
+        self.CheckBoxInput.addWidget(self.CheckboxAAAA)
+        self.CheckBoxInput.addWidget(self.CheckboxMx)
+
+        # result of search
+        self.Result = QTextBrowser()
+
+        self.Resultoutput = QVBoxLayout()
+        self.Resultoutput.addWidget(self.Result)
+
+        # layout components
+        self.Layout = QVBoxLayout()
+        self.Layout.addLayout(self.Header)
+        self.Layout.addSpacing(20)
+        self.Layout.addLayout(self.CheckBoxInput)
+        self.Layout.addSpacing(20)
+        self.Layout.addLayout(self.Resultoutput)
         self.setLayout(self.Layout)
