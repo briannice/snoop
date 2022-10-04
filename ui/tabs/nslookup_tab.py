18292cfa303af4c18ddcff11cb92f990c3f9784c
Maxim Massart <maxim.massart@student.kdg.be>
2022-10-04 22:50:51 +0200

    Connection between frontend and backend has been created. Possible to lookup DNS records in different ways.

--- ui/tabs/nslookup_tab.py
+++ ui/tabs/nslookup_tab.py
@@ -1,5 +1,8 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout, QHBoxLayout, QVBoxLayout, QLineEdit, QPushButton, QCheckBox, \
     QTextBrowser
+import dns.rrset, dns.rdatatype
+from lib.domainresearch.nslookup import lookupRecord
 
 
 class NslookupTab(QWidget):
@@ -22,16 +25,13 @@ class NslookupTab(QWidget):
         self.Header.addStretch(1)
 
         # possible options
-        self.CheckboxNs = QCheckBox("NS")
-        self.CheckboxA = QCheckBox("A")
-        self.CheckboxAAAA = QCheckBox("AAAA")
-        self.CheckboxMx = QCheckBox("MX")
+
+        self.Checkboxes = [QCheckBox("NS"), QCheckBox("A"), QCheckBox("AAAA"), QCheckBox("MX")]
 
         self.CheckBoxInput = QHBoxLayout()
-        self.CheckBoxInput.addWidget(self.CheckboxNs)
-        self.CheckBoxInput.addWidget(self.CheckboxA)
-        self.CheckBoxInput.addWidget(self.CheckboxAAAA)
-        self.CheckBoxInput.addWidget(self.CheckboxMx)
+
+        for x in range(len(self.Checkboxes)):
+            self.CheckBoxInput.addWidget(self.Checkboxes[x])
 
         # result of search
         self.Result = QTextBrowser()
@@ -47,3 +47,25 @@ class NslookupTab(QWidget):
         self.Layout.addSpacing(20)
         self.Layout.addLayout(self.Resultoutput)
         self.setLayout(self.Layout)
+
+        # event handlers
+        self.DomainButton.clicked.connect(self.handle_search_domain)
+
+    def handle_search_domain(self):
+        domain = self.DomainEdit.text()
+        self.Result.clear()
+
+        for checkbox in range(len(self.Checkboxes)):
+            if self.Checkboxes[checkbox].checkState() == Qt.Checked:
+                rdataCheckbox = dns.rdatatype.from_text(self.Checkboxes[checkbox].text())
+                self.handle_result_search_domain(lookupRecord(domain, rdataCheckbox))
+
+    def handle_result_search_domain(self, result: dns.rrset):
+        #todo: when MX type, value gets shown as well so maybe fix?
+        if result is not None:
+            for re in result:
+                typeData = dns.rdatatype.to_text(re.rdtype)
+                self.Result.append(typeData + ": " + str(re))
+        else:
+            #todo: fix to show what kind of record hasnt been found (right now return of None but need to change)
+            self.Result.append("No records found")
