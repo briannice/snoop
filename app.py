340852ea0ea69dbe2ff43c6681efb17a69a83f1b
Maxim Massart <maxim.massart@student.kdg.be>
2022-10-06 08:31:20 +0200

    Cleanup crew app.py

--- app.py
+++ app.py
@@ -16,20 +16,6 @@ class MainWindow(QMainWindow):
         self.RootWidget = RootWidget()
         self.setCentralWidget(self.RootWidget)
 
-
-# testing, will delete later
-from lib.domainresearch.nslookup import *
-
-res = lookupRecord("kdg.be", dns.rdatatype.AAAA)
-if res is not None:
-    for re in res:
-        if re.rdtype == dns.rdatatype.MX:
-            print(re.exchange)
-        else:
-            print(re)
-else:
-    print("No records found")
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
