d7dc98c647123bebf8870551246198bd85537403
briannice <brian.nys.dev@gmail.com>
2022-10-06 22:26:48 +0200

    fix multiprocessign error

--- workers/network_scanning_worker.py
+++ workers/network_scanning_worker.py
@@ -1,6 +1,5 @@
-# Standard lib
 from ipaddress import IPv4Address, IPv4Network
-from multiprocessing import Manager, Process
+from multiprocessing import Manager, Process, Pool
 from typing import List
 
 from PyQt5.QtCore import QRunnable, pyqtSlot
@@ -27,7 +26,7 @@ class NetworkScanningWorker(QRunnable):
         self,
         network: IPv4Network = IPv4Network("192.168.1.0/24")
     ):
-        super(NetworkScanningWorker, self).__init__()
+        super().__init__()
         self.network = network
         self.signals = WorkerSignals()
 
@@ -38,21 +37,27 @@ class NetworkScanningWorker(QRunnable):
         multiprocessing.
         """
 
+        pool = Pool(processes=100)
         manager = Manager()
         result = manager.list()
         tasks = []
         hosts = self.network.hosts()
 
         for host in hosts:
-            task = Process(
-                target=NetworkScanningWorker.run_task,
-                args=(host, result)
-            )
-            tasks.append(task)
-            task.start()
+            # task = Process(
+            #     target=NetworkScanningWorker.run_task,
+            #     args=(host, result)
+            # )
+            # tasks.append(task)
+            pool.apply_async(NetworkScanningWorker.run_task,
+                             args=(host, result))
+            # task.start()
 
-        for task in tasks:
-            task.join()
+        # for task in tasks:
+        #     task.join()
+
+        pool.close()
+        pool.join()
 
         self.signals.progress.emit(100)
         self.signals.finished.emit(True)
