d8be9a756b6bab6cdb43bc6f3c038eeb8b45178c
briannice <brian.nys.dev@gmail.com>
2022-10-07 17:53:30 +0200

    port and host scan results

--- lib/scanning/utils.py
+++ lib/scanning/utils.py
@@ -1,10 +1,11 @@
 from enum import Enum
+from ipaddress import IPv4Address
 
 
 class HostState(Enum):
     UP = "UP"
     DOWN = "DOWN"
-    UNNOWN = "UNKNOWN"
+    UNKNOWN = "UNKNOWN"
 
 
 class PortState(Enum):
@@ -14,3 +15,100 @@ class PortState(Enum):
     UNFILTERED = "UNFILTERED"
     OPEN_FILTERED = "OPEN | FILTERED"
     CLOSED_FILTERED = "CLOSED | FILTERED"
+
+
+class ICMPPacket():
+
+    def __init__(
+        self,
+        src_ip: IPv4Address,
+        dst_ip: IPv4Address,
+        type: int,
+        code: int,
+    ):
+        self.type = type
+        self.code = code
+        self.src_ip = src_ip
+        self.dst_ip = dst_ip
+
+
+class TCPPacket():
+
+    def __init__(
+        self,
+        src_ip: IPv4Address,
+        dst_ip: IPv4Address,
+        src_port: int,
+        dst_port: int,
+        flags: int,
+    ):
+        self.src_ip = src_ip
+        self.dst_ip = dst_ip
+        self.src_port = src_port
+        self.dst_port = dst_port
+        self.flags = flags
+
+
+class ICMPHostScanResult():
+
+    def __init__(
+        self,
+        state: HostState,
+        src_ip: IPv4Address,
+        dst_ip: IPv4Address,
+        type: int,
+        code: int,
+    ):
+        pkt = ICMPPacket(src_ip, dst_ip, type, code)
+
+        self.pkt = pkt
+        self.state = state
+
+
+class ICMPPortScanResult():
+
+    def __init__(
+        self,
+        state: PortState,
+        src_ip: IPv4Address,
+        dst_ip: IPv4Address,
+        type: int,
+        code: int,
+    ):
+        pkt = ICMPPacket(src_ip, dst_ip, type, code)
+
+        self.pkt = pkt
+        self.state = state
+
+
+class TCPHostScanResult():
+    def __init__(
+        self,
+        state: HostState,
+        src_ip: IPv4Address,
+        dst_ip: IPv4Address,
+        src_port: int,
+        dst_port: int,
+        flags: int,
+    ):
+        pkt = TCPPacket(src_ip, dst_ip, src_port, dst_port, flags)
+
+        self.pkt = pkt
+        self.state = state
+
+
+class TCPPortScanResult():
+
+    def __init__(
+        self,
+        state: PortState,
+        src_ip: IPv4Address,
+        dst_ip: IPv4Address,
+        src_port: int,
+        dst_port: int,
+        flags: int,
+    ):
+        pkt = TCPPacket(src_ip, dst_ip, src_port, dst_port, flags)
+
+        self.pkt = pkt
+        self.state = state
