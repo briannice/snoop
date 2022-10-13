30657817405090e477d41001071ecf4f985eb837
Maxim Massart <maxim.massart@student.kdg.be>
2022-10-14 00:09:46 +0200

    Backend connection added to new pattern + visual bugfix of MX record + show what kind of DNS record has no result

--- view/ns_lookup_view.py
+++ view/ns_lookup_view.py
@@ -1,6 +1,32 @@
 from ui import NslookupUi
+import dns.rrset, dns.rdatatype
+from lib.domainresearch import lookupRecord
 
 
 class NslookpView(NslookupUi):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Handlers
+        self.DomainButton.clicked.connect(self.handle_search_domain)
+
+    def handle_search_domain(self):
+        domain = self.DomainEdit.text()
+        self.Result.clear()
+
+        for checkbox in range(len(self.Checkboxes)):
+            # todo: voordien stond er Qt.Checked wat value 2 teruggaf, maar widgets hoe oplossen?
+            if self.Checkboxes[checkbox].checkState() == 2:
+                rdataCheckbox = dns.rdatatype.from_text(self.Checkboxes[checkbox].text())
+                self.handle_result_search_domain(lookupRecord(domain, rdataCheckbox), rdataCheckbox)
+
+    def handle_result_search_domain(self, result: dns.rrset, typeData: dns.rdatatype):
+        if result is not None:
+            for re in result:
+                if typeData == dns.rdatatype.MX:
+                    # todo: add option to show preference value anyway if multiple mail servers?
+                    self.Result.append(dns.rdatatype.to_text(typeData) + ": " + str(re.exchange))
+                else:
+                    self.Result.append(dns.rdatatype.to_text(typeData) + ": " + str(re))
+        else:
+            self.Result.append(dns.rdatatype.to_text(typeData) + ": " + "No records found")
