0e6b6e517f5a8bf2c9c78e595bdcd28c3b950a13
Maxim Massart <maxim.massart@student.kdg.be>
2022-10-14 19:47:31 +0200

    Added of recursive search of MX and NS records

--- view/ns_lookup_view.py
+++ view/ns_lookup_view.py
@@ -17,15 +17,29 @@ class NslookpView(NslookupUi):
         for checkbox in range(len(self.Checkboxes)):
             if self.Checkboxes[checkbox].isChecked():
                 rdataCheckbox = dns.rdatatype.from_text(self.Checkboxes[checkbox].text())
-                self.handle_result_search_domain(lookupRecord(domain, rdataCheckbox), rdataCheckbox)
+                self._handle_result_search_domain(lookupRecord(domain, rdataCheckbox), rdataCheckbox)
 
-    def handle_result_search_domain(self, result: dns.rrset, typeData: dns.rdatatype):
+    def _handle_result_search_domain(self, result: dns.rrset, typeData: dns.rdatatype):
         if result is not None:
             for re in result:
                 if typeData == dns.rdatatype.MX:
                     # todo: add option to show preference value anyway if multiple mail servers?
                     self.Result.append(dns.rdatatype.to_text(typeData) + ": " + str(re.exchange))
+
+                    # Search on IP adresses related to MX
+                    self._sub_search(lookupRecord(str(re.exchange), dns.rdatatype.A))
+                    self._sub_search(lookupRecord(str(re.exchange), dns.rdatatype.AAAA))
                 else:
                     self.Result.append(dns.rdatatype.to_text(typeData) + ": " + str(re))
+
+                    # Same as with MX, but on top of it for NS
+                    if typeData == dns.rdatatype.NS:
+                        self._sub_search(lookupRecord(str(re), dns.rdatatype.A))
+                        self._sub_search(lookupRecord(str(re), dns.rdatatype.AAAA))
         else:
             self.Result.append(dns.rdatatype.to_text(typeData) + ": " + "No records found")
+
+    def _sub_search(self, result: dns.rrset):
+        if result is not None:
+            for re in result:
+                self.Result.append("\t" + dns.rdatatype.to_text(re.rdtype) + ": " + str(re))
