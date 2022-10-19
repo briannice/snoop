3537f7a67d36bf3133b0e9fc78a0c1d14aee535b
briannice <brian.nys.dev@gmail.com>
2022-10-19 11:44:21 +0200

    fix window sizes

--- widgets/tab_widget.py
+++ widgets/tab_widget.py
@@ -6,4 +6,5 @@ class TabWidget(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.setFixedSize(1000, 800)
         self.setContentsMargins(25, 25, 25, 25)
